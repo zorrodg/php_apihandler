227015d8df8778dca9d54dbdb68913a2590b827b
Andrés Zorro <zorrodg@gmail.com>
2014-04-17 18:32:38 -0500

    Fixed xml response

---
+++ api/engine/Output.class.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Handles output and defines methods to format output.
+ *
+ * @package APIhandler
+ * @author Andrés Zorro <zorrodg@gmail.com>
+ * @version 0.1
+ * 
+ */
+
+class Output{
+	public static function set_headers($output = DEFAULT_OUTPUT){
+		header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
+        switch($output){
+            case 'json':
+                header("Content-Type: application/json");
+            	break;
+            case 'html':
+                header("Content-Type: text/html");
+            	break;
+            case 'xml':
+            	header('Content-Type: text/xml');
+            	break;
+        };
+	}
+
+	/**
+	 * Encodes data
+	 * @param  [any] 	$data   Data to output
+	 * @param  [string] $output Custom output to render. Defaults defined.
+	 * @return [string]         Encoded string
+	 */
+	static public function encode($data, $output = DEFAULT_OUTPUT){
+		self::set_headers($output);
+
+		switch ($output){
+			case "json":
+				return json_encode($data);
+			case "xml":
+                $output = '<?xml version="1.0" encoding="UTF-8"?><response>';
+                $output.= self::XML_encode($data);
+                $output.= '</response>';
+				return $output;
+		}
+	}
+
+    private function XML_encode($items){
+    	$output = '';
+    	if(!is_object($items) && !is_array($items))
+            $items = explode(',', $items);
+
+        foreach($items as $key => $item){
+            $output.='<'.$key.'>';
+            if(is_object($items) && !is_scalar($items)){
+                $output.=self::XML_encode($item);
+            }elseif(is_array($items) && !is_scalar($item) && !is_numeric($key) && !is_null($item)){
+                $output.=self::XML_encode($item);
+            } else {
+                $output.=$item;
+            }
+            $output.='</'.$key.'>';
+        }
+
+        return $output;
+    }
+}
