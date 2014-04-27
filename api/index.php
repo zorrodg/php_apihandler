77742bb6e99a19a20ba17b32148c52e8b9823824
Andrés Zorro <zorrodg@gmail.com>
2014-04-27 14:50:06 -0500

    Minor fixes

--- api/index.php
+++ api/index.php
@@ -9,31 +9,7 @@
  * @version 0.1
  * 
  */
+require_once "api.config.php";
+require_once "engine/api_start.php";
 
-//Database configuration
-define('DATABASE',"apihandlertest");
-define('DB_PREFIX',"api_");
-define('DB_USER',"root");
-define('DB_PASSWORD',"root");
-define('HOSTNAME',"127.0.0.1");
-define('DB_ENGINE',"mysql");
-
-//Set cache
-define('CACHE', true);
-define('CACHE_FOLDER', "/cache");
-define('CACHE_TIMEOUT', 3600);
-
-//Default output 
-define('DEFAULT_OUTPUT',"json");
-
-//Set endpoint files
-define('ENDPOINTS', serialize(array("Getters", "Posters", "Deleters")));
-
-//Set security
-define('SECURE_TYPE', "oauth");
-define('OAUTH_SERVICE', "self");
-define('OAUTH_CONSUMERS', serialize(array("test")));
-
-//Start API
-require_once("engine/api_start.php");
 ?>
--- api/oauth/index.php
+++ api/oauth/index.php
@@ -1,31 +1,26 @@
 <?php
-define('DATABASE',"apihandlertest");
-define('DB_PREFIX',"api_");
-define('DB_USER',"root");
-define('DB_PASSWORD',"root");
-define('HOSTNAME',"127.0.0.1");
-define('DB_ENGINE',"mysql");
-
+require_once "../api.config.php";
 require_once "../lib/oauth/OAuthStore.php";
 require_once "../lib/oauth/OAuthServer.php";
 
-if(DB_ENGINE === "mysql"){
-	$GLOBALS['oauth_connection'] = new mysqli(HOSTNAME, DB_USER, DB_PASSWORD, DATABASE);
-	$GLOBALS['oauth_store'] = OAuthStore::instance("MySQLi", array('conn' => $GLOBALS['oauth_connection']));
+if(OAUTH_SERVICE === "self"){
+	if(DB_ENGINE === "mysql"){
+		$GLOBALS['oauth_connection'] = new mysqli(HOSTNAME, DB_USER, DB_PASSWORD, DATABASE);
+		$GLOBALS['oauth_store'] = OAuthStore::instance("MySQLi", array('conn' => $GLOBALS['oauth_connection']));
+		$server = new OAuthServer();
+	}
+
 	$server = new OAuthServer();
+
+	switch($_REQUEST['oauth_type']){
+		case "request":
+			$server->requestToken();
+			exit();
+		case "authorize":
+			$server->authorizeVerify();
+			$server->authorizeFinish(TRUE, 1);
+			$server->accessToken();
+			exit();
+	}
 }
 
-
-
-$server = new OAuthServer();
-
-switch($_REQUEST['oauth_type']){
-	case "request":
-		$server->requestToken();
-		exit();
-	case "authorize":
-		$server->authorizeVerify();
-		$server->authorizeFinish(TRUE, 1);
-		$server->accessToken();
-		exit();
-}
