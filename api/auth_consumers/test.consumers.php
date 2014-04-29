99337741acebe5782d0fdc24704cd5c886a2b79f
Andrés Zorro <zorrodg@gmail.com>
2014-04-28 21:30:59 -0500

    Minor fixes

--- api/auth_consumers/test.consumers.php
+++ api/auth_consumers/test.consumers.php
@@ -4,6 +4,7 @@ $andres = new OAuth_Consumer(1,
 	"Andres Zorro", 
 	"zorrodg@gmail.com", 
 	"http://localhost/apihandler/api", 
-	"http://localhost/apihandler/api/callback.php", 
-	array("new" => TRUE)
+	"http://localhost/apihandler/api/callback.php"
 	);
+
+$server = new OAuth_Server($andres->get_consumer(), NULL);
--- api/engine/OAuth_Server.class.php
+++ api/engine/OAuth_Server.class.php
@@ -12,44 +12,32 @@ class OAuth_Server{
 		$filename = getcwd()."/engine/oauth_services/credentials/". $consumer['consumer_key'] .".txt";
 		$store = $GLOBALS['oauth_store'];
 
-		$server_location =  "//".$_SERVER['HTTP_HOST'];
+		$server_location =  "http://".$_SERVER['HTTP_HOST'];
 		$serveruri = $serveruri ?: $server_location."/api";
 
 		$credentials = @file_get_contents($filename);
 		if($credentials){
 			$credentials = explode(";", $credentials);
-			$consumer = array();
+			$server = array();
 			foreach($credentials as $c){
 				$cr = explode("=", $c);
 				$server[$cr[0]] = $cr[1];
 			}
-			if(isset($options['update'])){
-				$server = array(
-				    'consumer_key' => $consumer['consumer_key'],
-				    'consumer_secret' => $consumer['consumer_secret'],
-				    'server_uri' => ,
-				    'signature_methods' => array('HMAC-SHA1', 'PLAINTEXT'),
-				    'request_token_uri' => $options['request_token_uri'] ? $options['request_token_uri'] : $serveruri."/oauth/request",
-				    'authorize_uri' => $options['authorize_uri'] ? $options['authorize_uri'] : $serveruri."/oauth/authorize",
-				    'access_token_uri' => $options['access_token'] ? $options['access_token'] : $serveruri."/oauth/access",
-				);
-				
-				$key = $store->updateServer($server, $consumer['user_id']);
-			} else {
-				$key = $consumer['consumer_key'];
-			}
+			
+			$key = $consumer['consumer_key'];
 			$this->server = $store->getServer($key, $consumer['user_id']);
 
-		} elseif($options['new']){
+		} elseif(isset($options['new'])){
+
 			$arr = array();
 			$server = array(
 			    'consumer_key' => $consumer['consumer_key'],
 			    'consumer_secret' => $consumer['consumer_secret'],
-			    'server_uri' => ,
+			    'server_uri' => $serveruri,
 			    'signature_methods' => array('HMAC-SHA1', 'PLAINTEXT'),
-			    'request_token_uri' => $options['request_token_uri'] ? $options['request_token_uri'] : $serveruri."/oauth/request",
-			    'authorize_uri' => $options['authorize_uri'] ? $options['authorize_uri'] : $serveruri."/oauth/authorize",
-			    'access_token_uri' => $options['access_token'] ? $options['access_token'] : $serveruri."/oauth/access",
+			    'request_token_uri' => isset($options['request_token_uri']) ? $options['request_token_uri'] : $serveruri."/oauth/request",
+			    'authorize_uri' => isset($options['authorize_uri']) ? $options['authorize_uri'] : $serveruri."/oauth/authorize",
+			    'access_token_uri' => isset($options['access_token']) ? $options['access_token'] : $serveruri."/oauth/access",
 			);
 
 			$key = $store->updateServer($server, $consumer['user_id']);
